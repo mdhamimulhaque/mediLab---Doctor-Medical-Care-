c3eaffd24c81b72c1ce6f084e84106d7e7257fb3
md Hamimul Haue <mdhamimulhaque@gmail.com>
2023-12-12 21:43:57 +0600

    redux-tailwind config setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import Providers from "@/lib/providers";
 
 export const metadata: Metadata = {
   title: "MediLab",
@@ -16,7 +15,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Providers>
+        <body>{children}</body>
+      </Providers>
     </html>
   );
 }
